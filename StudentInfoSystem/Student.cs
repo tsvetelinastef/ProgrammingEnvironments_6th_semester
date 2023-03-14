8236508cee033a680a97a5c5dfcd425bbbac31d9
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-03-14 22:40:46 +0200

    changes

---
+++ StudentInfoSystem/Student.cs
@@ -0,0 +1,47 @@
+namespace StudentInfoSystem
+{
+    internal class Student
+    {
+        public Student()
+        {
+        }
+
+        public Student(string nameI, string middleNameI, string lastNameI, string facultyI, string majorI, string OKSI,
+            string studentStatusI, string facultyNumberI, string courseI, string potokI, string groupI)
+        {
+            Name = nameI;
+            MiddleName = middleNameI;
+            LastName = lastNameI;
+            Faculty = facultyI;
+            Major = majorI;
+            OKS = OKSI;
+            StudentStatus = studentStatusI;
+            FacultyNumber = facultyNumberI;
+            Course = courseI;
+            Potok = potokI;
+            Group = groupI;
+        }
+
+        public string Name { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Faculty { get; set; }
+
+        public string Major { get; set; }
+
+        public string OKS { get; set; }
+
+        public string StudentStatus { get; set; }
+
+        public string FacultyNumber { get; set; }
+
+        public string Course { get; set; }
+
+        public string Potok { get; set; }
+
+        public string Group { get; set; }
+    }
+}
---
+++ StudentInfoSystem/StudentData.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace StudentInfoSystem
+{
+    internal static class StudentData
+    {
+        private static List<Student> _testStudents;
+
+        public static List<Student> TestStudents
+        {
+            get
+            {
+                setFirstStudent();
+                return _testStudents;
+            }
+            private set { }
+        }
+
+        private static void setFirstStudent()
+        {
+            if (_testStudents == null)
+            {
+                _testStudents = new List<Student>();
+                var s1 = new Student("Tsvetelina", "Tsvetanova", "Stefanova", "FKST", "KSI", "bachelor", "regular",
+                    "121220181", "3", "10", "43");
+                _testStudents.Add(s1);
+            }
+        }
+    }
+}
